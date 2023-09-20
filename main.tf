d003ceb82219106174a288cd1aa7ceba1e705f1d
vatanas <145432474+vatanas@users.noreply.github.com>
2023-09-20 17:47:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ provider "google" {
 }
 
 //Configuring Kubernetes Provider
-//provider "kubernetes" {
-  //host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  //token = data.google_client_config.provider.access_token
- // cluster_ca_certificate = base64decode(
-  //  data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
-  //)
-//}
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.gke_cluster1.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.gke_cluster1.master_auth[0].cluster_ca_certificate,
+  )
+}
 
 
 //Creating VPC Network
